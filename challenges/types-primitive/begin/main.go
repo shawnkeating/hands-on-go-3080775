837a73cf54a7286e6c4cee06cbdd192d461bc2c5
Shawn Keating <shawn.keating@digicert.com>
2023-12-11 10:00:29 +0000

    modified:   challenges/types-primitive/begin/main.go modified:   types/constants/begin/main.go modified:   types/conversion/begin/main.go modified:   types/pointers/begin/main.go modified:   types/variables/begin/main.go

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,37 @@
 // challenges/types-primitive/begin/main.go
 package main
 
+import "fmt"
+
 // use type inference to create a package-level variable "val" and assign it a value of "global"
+var val = "global"
 
 func main() {
 	// create a local variable "val" and assign it an int value
+	val := 42
 
 	// print the value of the local variable "val"
+	fmt.Printf("%T, local val = %v\n", val, val)
 
 	// print the value of the package-level variable "val"
+	printGlobal()
 
 	// update the package-level variable "val" and print it again
+	updateGlobal()
+	printGlobal()
 
 	// print the pointer address of the local variable "val"
+	fmt.Printf("%T,local &val = %v\n", &val, &val)
 
 	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+	*(&val) = 100
+	fmt.Printf("local val = %v\n", val)
+}
+
+func printGlobal() {
+	fmt.Printf("global val = %v\n", val)
+}
+
+func updateGlobal() {
+	val = "updated global"
 }
--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,11 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+	var a = 1.5676
+	var b = uint(a)
+	fmt.Printf("%v=%T, %v=%T", a, a, b, b)
 }
